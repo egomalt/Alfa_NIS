d44168c27c58c64939e1f3d6452f2844641fc58e
egomalt <egomalt@mail.ru>
2026-06-13 23:04:57 +0300

    feat: add article reading page with votes

--- NIS/articles/articles_app/urls.py
+++ NIS/articles/articles_app/urls.py
@@ -4,3 +4,6 @@ from . import views
 urlpatterns = [
     path('articles/<int:article_id>/', views.article_read, name='article_read'),
 ]
+
+# re-exported for api/v1/urls.py
+api_article_vote = views.api_article_vote
